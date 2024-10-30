b27f1f779b04abb2ae84500690d6e1287e162ebc
Filip <fil.wolinski@gmail.com>
2024-10-31 00:55:14 +0100

    simplified file structure

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-import src
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/id/{product}")
+def displayProduct(product: int):
+    return f"product {product}"
